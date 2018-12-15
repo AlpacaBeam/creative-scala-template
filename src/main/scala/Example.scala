4bf907d2a3c4847994711fd836ead36a15e0b2ad
AlpacaBeam <45133072+AlpacaBeam@users.noreply.github.com>
2018-12-15 22:04:21 +0000

    Added more examples

--- src/main/scala/Example.scala
+++ src/main/scala/Example.scala
@@ -43,13 +43,19 @@ object Example {
 	box beside box beside box beside box beside box
   }
   
-  val aBox = Image.rectangle(40,40).fillColor(Color.royalBlue)
-  def boxes(count: Int): Image = 
-	count match {
-	  case 0 => Image.empty
-	  case n => aBox.beside(boxes(n-1))
-	  }
   
+  def boxes(count: Int): Image = {
+		val aBox = Image.rectangle(40,40).fillColor(Color.royalBlue)
+
+		def loop(count: Int) =
+		count match {
+			case 0 => Image.empty
+			case n => aBox.beside(loop(n-1))
+			}
+		
+		loop(count)
+	}
+
 	val aCircle: Image = circle(40)
   def cross(count: Int): Image = {
 		count match {
@@ -59,6 +65,61 @@ object Example {
   
 	}
   
+
+
+	def chessBoard(count: Int): Image = {
+		val chessBoardUnit: Image = (rectangle(40,40).fillColor(Color.red).beside(
+																	rectangle(40,40).fillColor(Color.black))).above(
+																rectangle(40,40).fillColor(Color.black).beside(
+																	rectangle(40,40).fillColor(Color.red)
+																		)
+																	)
+		
+		def loop(count: Int): Image =
+			count match {
+				case 0 => chessBoardUnit
+				case n => val unit = loop(n-1)
+									(unit beside unit) above (unit beside unit)
+			}
+		
+		loop(count)
+	}
+
+	def sierpinski(count: Int): Image = {
+		val aTriangle: Image = triangle(40,45).lineColor(Color.purple).lineWidth(2)
+		val base: Image = aTriangle above (aTriangle beside aTriangle)
+
+		count match {
+			case 0 => base
+			case n => val unit = sierpinski(n-1)
+								unit above (unit beside unit)
+		}	
+	}
+
+def growingBoxes(count: Int, size: Int): Image =
+  count match {
+    case 0 => Image.empty
+    case n => Image.rectangle(size, size) beside growingBoxes(n-1, size + 10)
+  }
+
+def gradientBoxes(count: Int): Image = {
+	val aBox: Image = rectangle(80,80).fillColor(Color.purple.spin((count*15).degrees)).lineColor(Color.red.spin((count*15).degrees)).lineWidth(4)
+
+	count match {
+		case 0 => aBox
+		case n => aBox beside gradientBoxes(n-1)
+	}
+}
+
+def concentricCircles(count: Int): Image = {
+	val aCircle = circle(50 + (count * 10)).lineColor(Color.royalBlue.spin((10*count).degrees)).lineWidth(5)
+	
+	count match {
+		case 0 => Image.empty
+		case n => concentricCircles(n-1) on aCircle
+	}
+}
+
   val image = boxes(Color.paleGoldenrod)
   def main(args: Array[String]): Unit = {
     image.draw
