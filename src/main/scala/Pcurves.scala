0d3301ece82859899f20d2dc3b9635cdaa702230
AlpacaBeam <45133072+AlpacaBeam@users.noreply.github.com>
2018-12-16 18:07:34 +0000

    Added higher order functions

--- src/main/scala/Pcurves.scala
+++ src/main/scala/Pcurves.scala
@@ -19,8 +19,43 @@ object Pcurves {
       on(dot.at(100, 100)).
       on(dot.at(100, 0))
 
-  val image: Image = circle(10)
+  def parametricCircle(angle: Angle): Point =
+    Point.polar(200, angle)
 
+  def sample(start: Angle, samples: Int): Image = {
+      //Angle.one is one complete turn. I.e. 360 degrees
+      val step = Angle.one / samples
+      val dot = triangle(10,10)
+      def loop(count: Int): Image = {
+          val angle = step * count
+          count match {
+              case 0 => Image.empty
+              case n =>
+                dot.at(parametricCircle(angle).toVec) on loop(n - 1)
+          }
+      }
+
+      loop(samples)
+  }
+
+  def rose(angle: Angle) = Point.polar((angle * 7).cos * 200, angle)
+
+  def concentricShapes(count: Int, singleShape: Int => Image): Image =
+    count match {
+        case 0 => Image.empty
+        case n => singleShape(n) on concentricShapes(n-1, singleShape)
+  }
+
+  val blackCircles: Image = concentricShapes(10, (n: Int) => Image.circle(50 + 5*n))
+
+  def redCircle(n: Int): Image = Image.circle(50 + 5*n).lineColor(Color.red)
+
+  val redCircles: Image = concentricShapes(10, redCircle _)
+
+  def colorCircle(n: Int): Image = Image.circle(80 + 10 * n).lineColor(Color.violet.spin((10*n).degrees)).lineWidth(7)
+  val colorCircles: Image = concentricShapes(40, colorCircle _)
+
+  val image: Image = circle(10)
   def main(args: Array[String]): Unit = {
     image.draw
   }
